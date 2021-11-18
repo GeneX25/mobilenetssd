13c4c0e4c730e316870f9e1e6910480316a4553f
GeneX25 <89238206+GeneX25@users.noreply.github.com>
2021-11-18 11:18:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,16 +151,40 @@ def event_handle(event):
         msg = str(event["message"]["text"])
         if (msg == "สวัสดี") :
             replyObj = TextSendMessage(text="ดีคั้บน้อง")
+            line_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "ทำไรอยู่") :
             replyObj = TextSendMessage(text="ทำไรก็ได้โตแย้ว อิอิ")
+            line_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "กินข้าวยัง") :
             replyObj = TextSendMessage(text="กินแล้วววววว")
+            line_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "มะยัง") :
             replyObj = TextSendMessage(text="ภันเต วิสุง วิสุง รักขะนัตถายะ")
+            line_bot_api.reply_message(rtoken, replyObj)
             
         else :
-            replyObj = TextSendMessage(text=msg)
-        line_bot_api.reply_message(rtoken, replyObj)
+            headers = request.headers
+            json_headers = json.dumps({k:v for k, v in headers.items()})
+            '''
+            json_line = request.get_json(force=False,cache=False)
+            json_line = json.dumps(json_line)
+            decoded = json.loads(json_line)
+            crl= pycurl.Curl()
+            crl.setopt( crl.URL, "https://bots.dialogflow.com/line/k--jomf/webhook")
+            crl.setopt( crl.POST, 1)
+            crl.setopt( crl.BINARYTRANSFER, true)
+            crl.setopt( crl.POSTFIELDS, decoded)
+            crl.setopt( crl.HTTPHEADER, json_headers)
+            crl.setopt( crl.SSL_VERIFYHOST, 2)
+            crl.setopt( crl.SSL_VERIFYPEER, 1)
+            crl.setopt( crl.FOLLOWLOCATION, 1)
+            crl.setopt( crl.RETURNTRANSFER, 1)
+            crl.perform()
+            crl.close()
+            '''
+            replyObj = TextSendMessage(text=json_headers)
+            line_bot_api.reply_message(rtoken, replyObj)
+           
     elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
